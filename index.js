d77abe56a79a8bcede68e468f7059c162fda7c43
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-09-07 15:09:35 +0300

    commit

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const { Telegraf } = require("telegraf");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: ".env" });
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
+
+// message initializations
+const welcomeMessage = `Welcome to back up bot..!  
+Send me a link... I promise to keep it safe`;
+
+const helpTxt = `Send me a link`;
+
+// basic commands
+bot.start((ctx) => ctx.reply(welcomeMessage));
+bot.help((ctx) => ctx.reply(helpTxt));
+
+// listen commands
 bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+
+bot.hears("new episode", (ctx) => {
+  console.log(ctx.from);
+  ctx.reply("new episode");
+});
+
 bot.launch();
