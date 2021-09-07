af5408a693344b66a2c80c1c6553f5a75cf33dcc
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-09-07 15:45:39 +0300

    commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+// pakages
 const { Telegraf } = require("telegraf");
 const dotenv = require("dotenv");
-
+const db = require("./db");
 dotenv.config({ path: ".env" });
 
+// initialize bot
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// message initializations
+// masic messages initializations
 const welcomeMessage = `Welcome to back up bot..!  
 Send me a link... I promise to keep it safe`;
-
 const helpTxt = `Send me a link`;
 
 // basic commands
@@ -18,9 +19,17 @@ bot.help((ctx) => ctx.reply(helpTxt));
 // listen commands
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 
-bot.hears("new episode", (ctx) => {
-  console.log(ctx.from);
-  ctx.reply("new episode");
+bot.hears(/new episode (.+)/, async (ctx) => {
+  const userId = ctx.from.id;
+  const episodeName = ctx.match[1];
+
+  // remove all old episodes
+  await db.remove({ userId }, { multi: true });
+  // create a new episode
+  await db.insert({ userId, episodeName, links: [] });
+
+  ctx.reply(`new episode created with name: ${episodeName}`);
 });
 
+// start bot
 bot.launch();
