4a070474afebcdc58c6b3477825707e674008a25
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-09-08 11:22:49 +0300

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align='center'>
+Links saving telegram bot
+</h1>
+
+### Feauture Improvements
+
+- Train to auto select category
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // pakages
-const { Telegraf } = require("telegraf");
-const dotenv = require("dotenv");
-const urlRegex = require("url-regex");
-const fetch = require("node-fetch");
-const db = require("./db");
+const { Telegraf, Markup } = require('telegraf');
+const dotenv = require('dotenv');
+const urlRegex = require('url-regex');
+const fetch = require('node-fetch');
+const ent = require('ent');
+const db = require('./db');
 
 // env config
-dotenv.config({ path: ".env" });
+dotenv.config({ path: '.env' });
 
 // initialize bot
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -14,19 +15,22 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 // regexs
 const titleRegex = /<title>(.+)<\/title>/gim;
 
+// memory storage
+const memoryStore = {};
+
 // masic messages initializations
 const welcomeMessage = `Welcome to back up bot..!  
 Send me a link... I promise to keep it safe`;
 const helpTxt = `Send me a link`;
 
 // basic commands
-bot.start((ctx) => ctx.reply(welcomeMessage));
-bot.help((ctx) => ctx.reply(helpTxt));
+bot.start(ctx => ctx.reply(welcomeMessage));
+bot.help(ctx => ctx.reply(helpTxt));
 
 // listen commands
-bot.on("sticker", (ctx) => ctx.reply("👍"));
+bot.on('sticker', ctx => ctx.reply('👍'));
 
-bot.hears(/new episode (.+)/, async (ctx) => {
+bot.hears(/new episode (.+)/, async ctx => {
   const userId = ctx.from.id;
   const episodeName = ctx.match[1];
 
@@ -39,25 +43,45 @@ bot.hears(/new episode (.+)/, async (ctx) => {
   ctx.reply(`New episode created with name: ${episodeName}`);
 });
 
-bot.hears(urlRegex(), async (ctx) => {
+bot.hears(urlRegex(), async ctx => {
   // get urls from texts
   const urls = ctx.message.text.match(urlRegex());
   const firstUrl = urls[0];
 
   // get url title
-  const body = await fetch(firstUrl).then((r) => r.text());
+  const body = await fetch(firstUrl).then(r => r.text());
   const titleTag = body.match(titleRegex);
   console.log(titleTag);
-  const title = titleTag.pop().replace("<title>", "").replace("</title>", "");
-
-  console.log(title);
+  const title = ent.decode(
+    titleTag.pop().replace('<title>', '').replace('</title>', '')
+  );
 
   // get userId
   const userId = ctx.from.id;
+  memoryStore[userId] = { url: firstUrl, title, category: '' };
 
-  await db.update({ userId }, { $push: { links: firstUrl } });
+  /*
+  await db.update(
+    { userId },
+    { $push: { links: { url: firstUrl, title, category: '' } } }
+  );
+*/
 
-  ctx.reply(`Link saved`);
+  ctx.reply(
+    `Ready to save: "${title}".
+  What category should it be?`,
+    Markup.keyboard(['/Articles', '/Releases', '/Lib', '/Silly'])
+      .oneTime()
+      .resize()
+    //   .extra()
+  );
+});
+
+bot.hears('/article', async ctx => {
+  const userId = ctx.from.id;
+  //   memoryStore[userId] = { url: firstUrl, title, category: '' };
+
+  ctx.reply(`Saving link: ${memoryStore[userId].title}`);
 });
 
 // start bot
